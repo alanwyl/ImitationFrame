7f48e2088ab2c01fe78ddf969ebecbacd1181471
Alan <yl.alanwong@gmail.com>
2018-12-13 07:04:20 +0800

    wip - resize image

--- app/src/main/java/hk/hku/cs/imitationframe/AutoFitImageView.java
+++ app/src/main/java/hk/hku/cs/imitationframe/AutoFitImageView.java
@@ -17,9 +17,6 @@ import android.widget.RelativeLayout;
  */
 public class AutoFitImageView extends AppCompatImageView implements View.OnTouchListener {
     private static final String TAG = "AW.AutoFitImageView";
-    private int mRatioWidth = 0;
-    private int mRatioHeight = 0;
-
     private int windowWidth, windowHeight;
 
     public AutoFitImageView(Context context) {
@@ -40,48 +37,14 @@ public class AutoFitImageView extends AppCompatImageView implements View.OnTouch
         windowHeight = metrices.heightPixels;
     }
 
-    /**
-     * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
-     * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
-     * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
-     *
-     * @param width  Relative horizontal size
-     * @param height Relative vertical size
-
-    public void setAspectRatio(int width, int height) {
-        if (width < 0 || height < 0) {
-            throw new IllegalArgumentException("Size cannot be negative.");
-        }
-        mRatioWidth = width;
-        mRatioHeight = height;
-        requestLayout();
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-        if (0 == mRatioWidth || 0 == mRatioHeight) {
-            setMeasuredDimension(width, height);
-        } else {
-            if (width < height * mRatioWidth / mRatioHeight) {
-                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
-            } else {
-                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
-            }
-        }
-    }
-
-     */
-
     public void reset() {
         RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)getLayoutParams();
         layoutParams.leftMargin = 0;
         layoutParams.topMargin = 0;
         layoutParams.bottomMargin = 0;
         layoutParams.rightMargin = 0;
-
+        setX(0);
+        setY(0);
     }
 
     private static final float MOVEMENT_THRESHOLD = 5.0f;
@@ -90,6 +53,8 @@ public class AutoFitImageView extends AppCompatImageView implements View.OnTouch
 
     private float lastX, lastY;
     private double lastDistance;
+    private int lastPointerCount;
+
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         if (this.getDrawable() == null) return false;
@@ -108,15 +73,16 @@ public class AutoFitImageView extends AppCompatImageView implements View.OnTouch
                     if (event.getPointerCount() == 1) {
                         x = (int) event.getRawX();
                         y = (int) event.getRawY();
+
                     } else if (event.getPointerCount() == 2) {
                         int p0 = event.getPointerId(0);
                         int p1 = event.getPointerId(1);
 
-                        x0 = (int) event.getX(p0);
-                        y0 = (int) event.getY(p0);
+                        x0 = (int) event.getX(p0) + lastX;
+                        y0 = (int) event.getY(p0) + lastY;
 
-                        x1 = (int) event.getX(p1);
-                        y1 = (int) event.getY(p1);
+                        x1 = (int) event.getX(p1) + lastX;
+                        y1 = (int) event.getY(p1) + lastY;
 
                         x = (x0 + x1)/2.0f;
                         y = (y0 + y1)/2.0f;
@@ -127,29 +93,21 @@ public class AutoFitImageView extends AppCompatImageView implements View.OnTouch
                     originalWidth = layoutParams.width;
                     if (originalHeight < 0) originalHeight = windowHeight;
                     if (originalWidth < 0) originalWidth = windowWidth;
-
-                    Log.d(TAG, "********** original " + originalHeight + "x" + originalWidth);
                     break;
 
                 case MotionEvent.ACTION_MOVE:
-                    if (event.getPointerCount() == 1) {
+                    if (event.getPointerCount() == 1 && lastPointerCount == 1) {
                         dx = (int)event.getRawX() - x;
                         dy = (int)event.getRawY() - y;
-
                         v.setX(lastX + dx);
                         v.setY(lastY + dy);
 
-                        Log.d(TAG, "********** points (" + (int)event.getRawX() + "," + (int)event.getRawY() + ")");
+                    } else if (event.getPointerCount() == 2 && lastPointerCount == 2) {
+                        x0 = (int) event.getX(0) + v.getX();
+                        y0 = (int) event.getY(0) + v.getY();
 
-                    } else if (event.getPointerCount() == 2) {
-                        int p0 = event.getPointerId(0);
-                        int p1 = event.getPointerId(1);
-
-                        x0 = (int) event.getX(p0);
-                        y0 = (int) event.getY(p0);
-
-                        x1 = (int) event.getX(p1);
-                        y1 = (int) event.getY(p1);
+                        x1 = (int) event.getX(1) + v.getX();
+                        y1 = (int) event.getY(1) + v.getY();
 
                         dx = (x0 + x1)/2.0f - x;
                         dy = (y0 + y1)/2.0f - y;
@@ -159,9 +117,8 @@ public class AutoFitImageView extends AppCompatImageView implements View.OnTouch
                         double ratio = distance / lastDistance;
                         Log.d(TAG, "********** ratio " + ratio);
 
-
-                        layoutParams.height = (int)(originalHeight * ratio);
                         layoutParams.width = (int)(originalWidth * ratio);
+                        layoutParams.height = (int)(originalHeight * ratio);
 
                         Log.d(TAG, "********** size " + layoutParams.width + "x" + layoutParams.height);
                         Log.d(TAG, "********** points (" + x0 + "," + y0 + ") (" + x1 + "," + y1 + ")");
@@ -173,12 +130,8 @@ public class AutoFitImageView extends AppCompatImageView implements View.OnTouch
                         v.invalidate();
                     }
 
-                    Log.d(TAG, "********** new position (" + v.getX() + "," + v.getY() + ")");
+                    lastPointerCount = event.getPointerCount();
 
-//                    if (dx > MOVEMENT_THRESHOLD || dy > MOVEMENT_THRESHOLD) {
-//
-//                    } else {
-//                        Log.d(TAG, "********** ignored");
 //                    }
 
 //                    v.setX(0); v.setY(0); // just try resizing first
