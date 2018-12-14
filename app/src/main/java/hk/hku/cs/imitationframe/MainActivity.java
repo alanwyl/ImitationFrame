3e4b361f53caebfce6244f6c6c10e8a5f4c19fc9
Alan Wong <yl.alanwong@gmail.com>
2018-12-14 22:58:46 +0800

    fixed permission problem causing crash on first run

--- app/src/main/java/hk/hku/cs/imitationframe/MainActivity.java
+++ app/src/main/java/hk/hku/cs/imitationframe/MainActivity.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     public static Context applicationContext;
     private static final int REQUEST_WRITE_STORAGE_REQUEST_CODE = 101;
@@ -23,10 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         requestAppPermissions();
 
-        if (null == savedInstanceState) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, Camera2BasicFragment.newInstance())
-                    .commit();
+        if (hasReadPermissions() && hasWritePermissions()) {
+            if (null == savedInstanceState) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, Camera2BasicFragment.newInstance())
+                        .commit();
+            }
         }
     }
 
@@ -48,5 +51,25 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean hasWritePermissions() {
         return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        boolean granted = true;
+        for (int i=0; i<grantResults.length; i++) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                granted = false;
+            }
+        }
+
+        if (!granted) {
+            Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+        } else {
+            if (hasReadPermissions() && hasWritePermissions()) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, Camera2BasicFragment.newInstance())
+                        .commit();
+            }
+        }
     }
 }
